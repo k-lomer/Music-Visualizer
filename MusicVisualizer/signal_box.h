bcc5b2aefcbf1d8a9edcbedafd6ec1ec70ec0dad
Kyle Lomer <kjl00@aub.edu.lb>
2020-10-25 22:20:38 +0000

    add comments to signal box class

--- MusicVisualizer/signal_box.h
+++ MusicVisualizer/signal_box.h
@@ -20,12 +20,26 @@ DEFINE_ENUM_FLAG_OPERATORS(SignalFlag)
 
 typedef std::vector<float> wave;
 
+// SignalBox is a class for handling signals
+// Provides processed signals for creating visual layers
 class SignalBox {
 public:
     SignalBox();
     ~SignalBox() {}
+
+    // Update the base signals
+    // param: new_signal - the raw signal to update values
     void update_signal(const wave & new_signal);
+
+    // Generate a wave based on the current stored signals
+    // param: signal_type - the flags describing the processing
+    // param: tapering - the fraction of the signal to taper to 0 at the ends
+    // return: wave - the newly generated wave
     wave gen_wave(SignalFlag signal_type, double tapering = 0.0) const;
+
+    // Get the maximum amplitude of the signal
+    // param: decay - whether to use the decayed updated signal or the raw signal
+    // return: float - the maximum value
     float get_max(bool decay) const;
     
 private:
